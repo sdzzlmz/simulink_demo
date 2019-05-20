696132bab5d972de95a4a7ac79603062fd32cdca
sdzzlmz <545411104@qq.com>
2019-05-20 14:50:30 +0800

    simulink_demo

---
+++ src/main/java/io/netty/transmit/TransmitServerHandler.java
@@ -0,0 +1,27 @@
+package io.netty.transmit;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+public class TransmitServerHandler extends ChannelInboundHandlerAdapter {
+
+	@Override
+	public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+		// TODO Auto-generated method stub
+		try {
+			System.out.println((ByteBuf)msg);
+		}
+		finally {
+			((ByteBuf)msg).release();
+		}
+
+	}
+
+	@Override
+	public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+		// TODO Auto-generated method stub
+		cause.printStackTrace();
+		ctx.close();
+	}
+
+}
